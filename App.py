3a35920eb3cc603ff1c95a74faf97eca147defd8
Larssippel <99999966+Larssippel@users.noreply.github.com>
2025-08-24 22:37:46 -0400

    indent

--- App.py
+++ App.py
@@ -14,7 +14,7 @@ import random
 import requests
 
 def loadPage(url):
-     headers = {
+    headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                       "AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/115.0 Safari/537.36",
